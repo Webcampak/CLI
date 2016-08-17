97021d9476c7e7a5622b74c033ab5cf013396d90
Francis G <francis@webcampak.com>
2016-08-16 23:14:08 -0400

    Updated files with gitignore

--- build/lib.linux-x86_64-2.7/tests/cli/plugins/test_example.py
+++
@@ -1,8 +0,0 @@
-"""Tests for Example Plugin."""
-
-from webcampak.utils import test
-
-class ExamplePluginTestCase(test.wpakTestCase):
-    def test_load_example_plugin(self):
-        self.app.setup()
-        self.app.plugin.load_plugin('example')
--- start-dev.sh
+++
@@ -1,6 +0,0 @@
-cd /home/webcampak/webcampak/bin/webcampak/
-virtualenv /home/webcampak/webcampak/bin/webcampak/env
-source /home/webcampak/webcampak/bin/webcampak/env/bin/activate
-pip install -r requirements.txt
-python setup.py develop
-webcampak --help
