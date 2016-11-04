9f509ad68370c156485c2caaaa70d628454d167d
Fgerthoffert <francois@webcampak.com>
2016-11-04 18:53:53 -0400

    Added support for Harbortronics filenames

--- webcampak/core/capture/drivers/wpakCaptureIPCam.py
+++ webcampak/core/capture/drivers/wpakCaptureIPCam.py
@@ -119,7 +119,7 @@ class captureIPCam(object):
                 return None
 
             self.currentCaptureDetails.setCaptureValue('captureDate', currentFileTime.isoformat())
-            self.captureFilename = fileName
+            self.captureFilename = currentFileTime.strftime("%Y%m%d%H%M%S") + ".jpg"
 
             # Move file to root of tmp directory
             self.log.info(
