db87c3e860e0535b0b8fca93bad31dd5966ced2e
fgerthoffert <francois@webcampak.com>
2017-01-03 08:41:59 -0500

    Updated set/get phidgets output to boolean

--- webcampak/core/wpakPhidgetsUtils.py
+++ webcampak/core/wpakPhidgetsUtils.py
@@ -38,14 +38,10 @@ class phidgetsUtils(object):
             if phidgetPort == "":
                 phidgetPort = 0
             phidgetsClass = phidgets(self)
-            outputValue = phidgetsClass.setOutputValue(phidgetPort, 0)
-            self.log.info("phidgetsUtils.restartCamera(): " + _("Phidget port set to:  %(outputValue)s") % {
-                'outputValue': str(outputValue)})
+            outputValue = phidgetsClass.setOutputValue(phidgetPort, False)
+            self.log.info("phidgetsUtils.restartCamera(): " + _("Phidget port set to: %(outputValue)s") % {'outputValue': str(outputValue)})
             if outputValue != 0:
                 self.log.error("phidgetsUtils.restartCamera(): " + _("Error Unable to set port to 0"))
             time.sleep(5)
-            outputValue = phidgetsClass.setOutputValue(phidgetPort, 1)
-            self.log.info("phidgetsUtils.restartCamera(): " + _("Phidget port set to:  %(outputValue)s") % {
-                'outputValue': str(outputValue)})
-            if outputValue != 1:
-                self.log.error("phidgetsUtils.restartCamera(): " + _("Error Unable to set port to 1"))
+            outputValue = phidgetsClass.setOutputValue(phidgetPort, True)
+            self.log.info("phidgetsUtils.restartCamera(): " + _("Phidget port set to: %(outputValue)s") % {'outputValue': str(outputValue)})
