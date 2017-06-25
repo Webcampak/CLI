e228eaee3f34a67be8fe153f32fb980f27a886c0
fgerthoffert <f.gerthoffert@gmail.com>
2017-06-25 12:12:19 -0400

    Corrected default test case

--- tests/cli/plugins/test_example.py
+++ tests/cli/plugins/test_example.py
@@ -5,4 +5,4 @@ from webcampak.utils import test
 class ExamplePluginTestCase(test.wpakTestCase):
     def test_load_example_plugin(self):
         self.app.setup()
-        self.app.plugin.load_plugin('example')
+        self.app.plugin.load_plugin('phidgets')
