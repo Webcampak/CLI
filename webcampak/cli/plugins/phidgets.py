a1e20e2a68f4b932cd733ebd0d23710dc8380d2c
fgerthoffert <f.gerthoffert@gmail.com>
2017-06-25 01:10:06 -0400

    Rename de camera reboot plugin

--- webcampak/cli/plugins/phidgets.py
+++ webcampak/cli/plugins/phidgets.py
@@ -7,17 +7,17 @@ import gettext
 from webcampak.core.wpakConfigObj import Config
 from webcampak.core.wpakPhidgetsUtils import phidgetsUtils
 
-def camera_plugin_hook(app):
+def phidgets_plugin_hook(app):
     # do something with the ``app`` object here.
     pass
 
 class ExamplePluginController(CementBaseController):
     class Meta:
         # name that the controller is displayed at command line
-        label = 'camera'
+        label = 'phidgets'
 
         # text displayed next to the label in ``--help`` output
-        description = 'Commands directly associated with camera operation'
+        description = 'Performs different operations on phidget boards'
 
         # stack this controller on-top of ``base`` (or any other controller)
         stacked_on = 'base'
@@ -26,12 +26,12 @@ class ExamplePluginController(CementBaseController):
         stacked_type = 'nested'
 
         # these arguments are only going to display under
-        # ``$ webcampak camera --help``
+        # ``$ webcampak phidgets --help``
         arguments = [
             (
                 ['-s', '--sourceid'],
                 dict(
-                    help='Source ID to camera from',
+                    help='Source ID of the source to operate on',
                     action='store',
                     )
             )
@@ -79,25 +79,9 @@ class ExamplePluginController(CementBaseController):
             self.phidgetsUtils = phidgetsUtils(self)
             self.phidgetsUtils.restartCamera()
 
-
-    @expose(help="Get camera status")
-    def status(self):
-        self.app.log.info("Geting camera status", __file__)
-        if self.app.pargs.config_dir != None:
-            config_dir = self.app.pargs.config_dir
-        else:
-            config_dir = self.app.config.get('webcampak', 'config_dir')
-        try:
-            print("test")
-            #collect = statsCollect(self.app.log, self.app.config, config_dir)
-            #collect.run()
-        except Exception:
-            self.app.log.fatal("Ooops! Something went terribly wrong, stack trace below:", exc_info=True)
-            raise
-     
 def load(app):
     # register the plugin class.. this only happens if the plugin is enabled
     handler.register(ExamplePluginController)
 
     # register a hook (function) to run after arguments are parsed.
-    hook.register('post_argument_parsing', camera_plugin_hook)
+    hook.register('post_argument_parsing', phidgets_plugin_hook)
