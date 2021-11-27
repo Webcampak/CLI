e67b6adea9d174d7f7641a5e7c2c0ea33dd9ad6c
Francois G <f.gerthoffert@gmail.com>
2021-11-26 19:07:58 -0500

    Update wpakFileUtils.py

--- webcampak/core/wpakFileUtils.py
+++ webcampak/core/wpakFileUtils.py
@@ -140,7 +140,7 @@ class fileUtils(object):
     @staticmethod
     def sizeof_fmt(num, suffix="B"):
         if num != None:
-            num = int(num)
+            num = int(str(num))
             for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
                 if abs(num) < 1024.0:
                     return "%3.1f%s%s" % (num, unit, suffix)
